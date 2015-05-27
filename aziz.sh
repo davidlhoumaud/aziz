2831386d19269343383f1e57b4149abf8e973e4c
BeHuman <craft@ckdevelop.org>
2015-05-27 14:52:13 +0200

    Mise à jour de la source

--- aziz.sh
+++ aziz.sh
@@ -24,49 +24,49 @@ function action() {
         zenity --notification --window-icon="info" --text="Aziz va mettre à jour le système via APT"
         zenity --question --title="Aziz" --window-icon="info" --text="Voulez-vous vraiment continuer ?"
         if [[ $? -eq 0 ]]; then
-            $TERM -e "sudo apt-get update;sudo apt-get upgrade;sudo apt-get dist-upgrade"
+            xterm -e "sudo apt-get update;sudo apt-get upgrade;sudo apt-get dist-upgrade"
         fi
     ;;
     2 ) echo "Je vais supprimer les paquets inutilisés de APT"
         zenity --notification --window-icon="info" --text="Aziz va supprimer les paquets inutilisés de APT"
         zenity --question --title="Aziz" --window-icon="info" --text="Voulez-vous vraiment continuer ?"
         if [[ $? -eq 0 ]]; then
-            $TERM -e sudo apt-get autoremove
+            xterm -e sudo apt-get autoremove
         fi
     ;;
     3 ) echo "Je vais nettoyer APT"
         zenity --notification --window-icon="info" --text="Aziz va nettoyer APT"
         zenity --question --title="Aziz" --window-icon="info" --text="Voulez-vous vraiment continuer ?"
         if [[ $? -eq 0 ]]; then
-            $TERM -e "sudo apt-get autoclean;sudo apt-get clean"
+            xterm -e "sudo apt-get autoclean;sudo apt-get clean"
         fi
     ;;
     4 ) echo "Je vais vider la corbeille"
         zenity --notification --window-icon="info" --text="Aziz va vider la corbeille"
         zenity --question --title="Aziz" --window-icon="info" --text="Voulez-vous vraiment continuer ?"
         if [[ $? -eq 0 ]]; then
-            $TERM -e "rm -r -f ~/.local/share/Trash/files/*"
+            xterm -e "rm -r -f ~/.local/share/Trash/files/*"
         fi
     ;;
     5 ) echo "Je vais supprimer les miniatures des images"
         zenity --notification --window-icon="info" --text="Aziz va supprimer les miniatures des images"
         zenity --question --title="Aziz" --window-icon="info" --text="Voulez-vous vraiment continuer ?"
         if [[ $? -eq 0 ]]; then
-            $TERM -e "find ~/.thumbnails -type f -atime +7 -delete"
+            xterm -e "find ~/.thumbnails -type f -atime +7 -delete"
         fi
     ;;
     6 ) echo "Je vais supprimer les fichiers de sauvegarde"
         zenity --notification --window-icon="info" --text="Aziz va supprimer les fichiers de sauvegarde"
         zenity --question --title="Aziz" --window-icon="info" --text="Voulez-vous vraiment continuer ?"
         if [[ $? -eq 0 ]]; then
-            $TERM -e "find ~/ -name '*~' -print0 | xargs -0 rm"
+            xterm -e "find ~/ -name '*~' -print0 | xargs -0 rm"
         fi
     ;;
     7 ) echo "Je vais supprimer les fichiers temporaires d'Adobe Flash Player"
         zenity --notification --window-icon="info" --text="Aziz va supprimer les fichiers temporaires d'Adobe Flash Player"
         zenity --question --title="Aziz" --window-icon="info" --text="Voulez-vous vraiment continuer ?"
         if [[ $? -eq 0 ]]; then
-            $TERM -e "rm -r ~/.adobe/Flash_Player;rm -r ~/.macromedia/Flash_Player"
+            xterm -e "rm -r ~/.adobe/Flash_Player;rm -r ~/.macromedia/Flash_Player"
         fi
     ;;
     esac
